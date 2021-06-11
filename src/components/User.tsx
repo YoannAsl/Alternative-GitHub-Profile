d70be310c96bbce959122b7ca0e987c62f742129
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-06-12 01:14:09 +0200

    Added all API calls

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,30 +1,59 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import axios from 'axios';
+import GhPoliglot from 'gh-polyglot';
 
 const User = () => {
 	const params: { username: string } = useParams();
-	const [repos, setRepos] = useState([]);
 
+	const [user, setUser] = useState([]);
+	const [repos, setRepos] = useState([]);
+	const [languages, setLanguages] = useState([]);
+
+	// Gets user infos
 	useEffect(() => {
 		const getUserInfos = async () => {
-			const req = await axios.get(
-				`https://api.github.com/users/${params.username}`
-			);
-			console.log(req.data);
+			try {
+				const req = await axios.get(
+					`https://api.github.com/users/${params.username}`
+				);
+				setUser(req.data);
+
+				console.log(req.data);
+			} catch (error) {
+				console.error(error);
+			}
 		};
 		getUserInfos();
 	}, [params.username]);
 
+	// Gets user repositories
 	useEffect(() => {
 		const getUserRepos = async () => {
-			const req = await axios.get(
-				`https://api.github.com/users/${params.username}/repos?per_page=100`
-			);
-			setRepos(req.data);
+			try {
+				const req = await axios.get(
+					`https://api.github.com/users/${params.username}/repos?per_page=100`
+				);
+				setRepos(req.data);
+
+				console.log(req.data);
+			} catch (error) {
+				console.error(error);
+			}
 		};
 		getUserRepos();
 	}, [params.username]);
+
+	// Gets languages used by user
+	useEffect(() => {
+		const me = new GhPoliglot(`${params.username}`);
+		me.userStats((err: any, stats: any) => {
+			if (err) console.error('Error', err);
+
+			setLanguages(stats);
+			console.log(stats);
+		});
+	});
 
 	return (
 		<div>
